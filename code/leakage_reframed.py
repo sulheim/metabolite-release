c9ddfe78f86530637bbc6797b789349eeea8c844
Snorre Sulheim <ssulheim@gmail.com>
2024-08-16 17:04:10 +0200

    Fixed estimation of shadow price for mets being already secreted. And redid yeast and ecoli rates

--- code/leakage_reframed.py
+++ code/leakage_reframed.py
@@ -9,6 +9,8 @@ import reframed
 def estimate_shadow_prices(model, constraints, intracellular_only = True, delta = 0.01, metabolites = []):
     intracellular_only = True
     solution = reframed.FBA(model, constraints=constraints)
+    # Check if metabolite is already being secreted
+
     wt_growth_rate = solution.fobj
     shadow_prices = {}
     if not len(metabolites):
@@ -22,6 +24,7 @@ def estimate_shadow_prices(model, constraints, intracellular_only = True, delta 
             temp.add_reaction_from_str(f'DM_{m_id}: {m_id} -->  [0, 0]')
 
     for i, m_id in enumerate(metabolites):
+        predicted_flux, r_secretion = get_predicted_metabolite_secretion(model, solution, m_id)
         # print(i, m_id)
         m = model.metabolites[m_id]
         if intracellular_only:
@@ -31,6 +34,9 @@ def estimate_shadow_prices(model, constraints, intracellular_only = True, delta 
             continue
         sp_constraints = {f'DM_{m_id}': (delta, 10)}
         sp_constraints.update(constraints)
+        if predicted_flux != 0:
+            sp_constraints.update({r_secretion:(predicted_flux, predicted_flux)})
+
         sp_solution = reframed.FBA(temp, constraints=sp_constraints)
         if isinstance(sp_solution.fobj, float):
             shadow_prices[m_id] = (sp_solution.fobj-wt_growth_rate)/delta
@@ -38,3 +44,23 @@ def estimate_shadow_prices(model, constraints, intracellular_only = True, delta 
             shadow_prices[m_id] = np.nan
         
     return shadow_prices
+
+
+def get_predicted_metabolite_secretion(model, solution, m_id):
+    mc_name = model.metabolites[m_id].name.split(" ")[0]
+    predicted_flux = 0
+    r_id = None
+    for r_id in model.get_exchange_reactions():
+        flux = solution.values[r_id]
+        r = model.reactions[r_id]
+        if  np.abs(flux) >1e-3:
+            # print(r_id, r.name, flux, r.lb, r.ub)
+            me_id = r.get_substrates()
+            if len(me_id):
+                me_name = model.metabolites[me_id[0]].name.split(' ')[0]
+                if me_name == mc_name:
+                    predicted_flux = flux
+                    break
+    return predicted_flux, r_id
+                
+    
