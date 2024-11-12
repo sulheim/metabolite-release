27bc3aae4b3fa9c4bd781590378df776b24b5af5
Snorre Sulheim <ssulheim@gmail.com>
2024-11-12 19:21:20 +0100

    broken pc

--- code/test_dFBA.py
+++
@@ -1,4 +0,0 @@
-def test_dfba():
-
-if __name__ == '__main__':
-	pass
--- code/test_growth.py
+++
@@ -1,8 +0,0 @@
-import cobra
-
-def check_fva_exchange_reactions(model):
-    
-if __name__ == '__main__':
-    model = cobra.io.read_sbml_model('momentiJO1366.xml')
-    solution = model.optimize()
-    print(model.summary())
