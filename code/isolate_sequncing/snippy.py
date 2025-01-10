0fd0286b7276effe87f9d88745934cd09805c860
nahanaoo <ericsven.u@gmail.com>
2025-01-10 14:55:51 +0100

    code for sequencing analsysi in Snakemake worfklow language

---
+++ code/isolate_sequncing/snippy.py
@@ -0,0 +1,10 @@
+from os.path import join
+import pandas as pd
+import os
+
+# Parsing annotated fixed mutations
+dirs = os.listdir("../../data/isolate_sequencing/")
+for dir in dirs:
+    f = join("..", "..", "data", "isolate_sequencing", dir, "snippy", "snps.tab")
+    df = pd.read_csv(f, sep="\t")
+    df.to_csv(join("results", "fixed_mutations", dir + ".tsv"), index=False, sep="\t")
