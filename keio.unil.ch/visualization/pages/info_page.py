8e921f1de3849a4e3a717d9538145e5ac808dfd0
nahanoo <ericsven.u@gmail.com>
2025-11-21 10:01:30 +0100

    left aligned logo

--- keio.unil.ch/visualization/pages/info_page.py
+++ keio.unil.ch/visualization/pages/info_page.py
@@ -1,11 +1,13 @@
 import dash
 from dash import dcc, html, callback
 
+
 # Load markdown content
 def load_markdown_content(filepath):
-    with open(filepath, 'r') as f:
+    with open(filepath, "r") as f:
         content = f.read()
     return content
+
 
 # Register the new page
 dash.register_page(__name__, path="/info", name="About", order=2)
@@ -13,7 +15,18 @@ dash.register_page(__name__, path="/info", name="About", order=2)
 layout = html.Div(
     [
         # html.H2("Information Page"),
-        html.Img(src='assets/illustration.png', style={'width': '40%', 'height': 'auto'}),
-        dcc.Markdown(load_markdown_content('data/info.md'))
-    ],style={"justify-content": "center", "align-items": "center", "display": "flex", "flex-direction": "column"}
+        html.Div(
+            html.Img(
+                src="assets/illustration.png", style={"width": "40%", "height": "auto"}
+            ),
+            style={"textAlign": "left"},
+        ),
+        dcc.Markdown(load_markdown_content("data/info.md")),
+    ],
+    style={
+        "justify-content": "center",
+        "align-items": "center",
+        "display": "flex",
+        "flex-direction": "column",
+    },
 )
